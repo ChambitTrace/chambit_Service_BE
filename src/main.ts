0826a9c379a78164af01a6e03ccbaf2de4201bbe
Eunji <hyni03@naver.com>
2025-07-24 13:57:35 +0000

    Init: Swagger 설정 (#1)

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { WinstonLoggerService } from './core/interceptors/logging/winston-logger
 import { CustomValidationPipe } from './core/pipes/validation';
 import { GlobalExceptionFilter } from './core/filter/global.filter';
 import { ValidationExceptionFilter } from './core/filter/validation.filter';
+import { SwaggerConfig } from './config/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,8 +14,14 @@ async function bootstrap() {
   app.useLogger(logger);
 
   // ValidationPipe 설정
-  app.useGlobalFilters(new GlobalExceptionFilter(logger), new ValidationExceptionFilter());
+  app.useGlobalFilters(
+    new GlobalExceptionFilter(logger),
+    new ValidationExceptionFilter(),
+  );
   app.useGlobalPipes(new CustomValidationPipe());
+
+  // Swagger 설정
+  SwaggerConfig.setup(app);
 
   await app.listen(process.env.BE_PORT ?? 3000);
 }
